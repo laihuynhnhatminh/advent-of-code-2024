1f39e32c834d2acd824c39c39cc5c7116a22a32e
Nhat Minh (Himemiya Taiyou) <nhatminhwork1996@gmail.com>
2024-12-05 19:05:48 +0700

    feat: day-3

---
+++ src/day-3/main.ts
@@ -0,0 +1,8 @@
+import { runSolutionWithPerformanceForLoops } from '../utils';
+import { solutionPart1, solutionPart2 } from './solution';
+
+// Solution for part 1
+runSolutionWithPerformanceForLoops(solutionPart1, 20, 5);
+
+// Solution for part 2
+runSolutionWithPerformanceForLoops(solutionPart2, 20, 5);
---
+++ src/day-3/solution.ts
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import path from 'path';
+const input = fs
+  .readFileSync(path.resolve('./src/day-3/data.txt'), 'utf8')
+  .trim();
+
+function mul(a: number, b: number): number {
+  return a * b;
+}
+
+export function solutionPart1() {
+  const newRegex = /mul\(\d+,\d+\)/g;
+  const mulRegex = /mul\((\d+),(\d+)\)/g;
+
+  return input
+    .match(newRegex)!
+    .toString()
+    .replace(mulRegex, '$1,$2')
+    .split(',')
+    .reduce((acc: number, val, index, arr) => {
+      if (index % 2 === 1) return acc;
+
+      acc += mul(Number(arr[index]), Number(arr[index + 1]));
+      return acc;
+    }, 0);
+}
+
+export function solutionPart2() {
+  const newRegex = /mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g;
+  const mulRegex = /mul\((\d+),(\d+)\)/g;
+  let isSkipping = false;
+
+  return input
+    .match(newRegex)!
+    .toString()
+    .replace(mulRegex, '$1,$2')
+    .split(',')
+    .map((v) => {
+      if (v === "don't()") {
+        isSkipping = true;
+      } else if (v === 'do()') {
+        isSkipping = false;
+      }
+
+      if (isSkipping) {
+        return NaN;
+      }
+
+      return Number(v);
+    })
+    .filter(v => !Number.isNaN(v))
+    .reduce((acc: number, _val, index, arr) => {
+      if (index % 2 === 1) return acc;
+
+      acc += mul(arr[index], arr[index + 1]);
+      return acc;
+    }, 0);
+}
